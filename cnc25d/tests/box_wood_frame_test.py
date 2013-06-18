4192946eadca55a719bf861cb7c69491f79b6ba1
charlyoleg <charlyoleg@fabfolk.com>
2013-06-18 12:40:58 +0200

    rename maco_test into test

--- cnc25d/tests/box_wood_frame_test.py
+++ cnc25d/tests/box_wood_frame_test.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 #
-# box_wood_frame_macro_test.py
+# box_wood_frame_test.py
 # test the box_wood_frame_macro.py which is included in bin/cnc25d_example_generator.py
 # created by charlyoleg on 2013/06/07
 # license: CC BY SA 3.0
 # 
 
 """
-box_wood_frame_macro_test.py lets run the script box_wood_frame_macro.py with the appropriate sys.path
+box_wood_frame_test.py lets run the script box_wood_frame_macro.py with the appropriate sys.path
 Running this script is a quick test to check that the package cnc25d code is running.
 """
 
