403e4d58e5ca03d75b22317a410e575113dd8f77
charlyoleg <charlyoleg@fabfolk.com>
2013-12-13 09:46:20 +0100

    [release] next release number

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='Cnc25D',
-  version='0.1.9',
+  version='0.1.10',
   author='charlyoleg',
   author_email='charlyoleg@fabfolk.com',
   #packages=['cnc25d', 'cnc25d.tests'],
